5c8f590c8e9579021a93e1f305cc9e9eb23a797c
hyk <2453778348@qq.com>
2022-02-28 16:05:08 +0800

    静态登录页面

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,22 +2,31 @@ import MSite from "@/pages/MSite/MSite"
 import Order from "@/pages/Order/Order"
 import Profile from "@/pages/Profile/Profile"
 import Search from "@/pages/Search/Search"
+import Login from "@/pages/Login/Login"
 export default [
     {
         path:'/msite',
-        component:MSite
+        component:MSite,
+        meta:{isShowFoot:true}
     },
     {
         path:'/search',
-        component:Search
+        component:Search,
+        meta:{isShowFoot:true}
     },
     {
         path:'/order',
-        component:Order
+        component:Order,
+        meta:{isShowFoot:true}
     },
     {
         path:'/profile',
-        component:Profile
+        component:Profile,
+        meta:{isShowFoot:true}
+    },
+    {
+        path:'/login',
+        component:Login
     },
     {
         path:'/',
