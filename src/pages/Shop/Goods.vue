4b5eae8643a3cd6b7a4feb6a9697b498d17595be
hyk <2453778348@qq.com>
2022-03-07 00:36:43 +0800

    better-scroll

--- src/pages/Shop/Goods.vue
+++ src/pages/Shop/Goods.vue
@@ -1,15 +1,220 @@
 <template>
   <div>
-      Goods
+    <div class="goods">
+      <div class="menu-wrapper" ref="left">
+        <ul ref="leftUl">
+          <!-- current -->
+          <li class="menu-item" v-for="(good, index) in goods" 
+            :key="good.name" :class="{current: currentIndex===index}" @click="selectItem(index)">
+            <span class="text bottom-border-1px">
+              <img class="icon" :src="good.icon" v-if="good.icon">
+              {{good.name}}
+            </span>
+          </li>
+        </ul>
+      </div>
+      <div class="foods-wrapper" ref="right">
+        <ul ref="rightUl">
+          <li class="food-list-hook" v-for="(good, index) in goods" :key="good.name">
+            <h1 class="title">{{good.name}}</h1>
+            <ul>
+              <li class="food-item bottom-border-1px" v-for="(food, index) in good.foods" 
+                :key="food.name" @click="showFood(food)">
+                <div class="icon">
+                  <img width="57" height="57" :src="food.icon">
+                </div>
+                <div class="content">
+                  <h2 class="name">{{food.name}}</h2>
+                  <p class="desc">{{food.description}}</p>
+                  <div class="extra">
+                    <span class="count">月售{{food.sellCount}}份</span>
+                    <span>好评率{{food.rating}}%</span></div>
+                  <div class="price">
+                    <span class="now">￥{{food.price}}</span>
+                    <span class="old" v-if="food.oldPrice">￥{{food.oldPrice}}</span>
+                  </div>
+                  <div class="cartcontrol-wrapper">
+                    <!-- <CartControl :food="food"/> -->
+                    CartControl
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <!-- <ShopCart/> -->
+    </div>
+    <!-- <Food :food="food" ref="food"/> -->
   </div>
 </template>
 
+
 <script type="text/ecmascript-6">
+import { mapState } from "vuex";
+import betterScroll from "@better-scroll/core"
   export default {
+      data() {
+          return {
+              scrollY:0,
+              tops:[]
+          }
+      },
+      computed:{
+          ...mapState(['goods']),
+          currentIndex(){
+            const {scrollY,tops} = this
+            return tops.findIndex((item,index)=>this.scrollY>=item && this.scrollY<tops[index+1])
+          }
+      },
+      // mounted(){
+      //   this.initTops()
+      // },
+      watch:{
+        goods(){
+          this.$nextTick(()=>{
+            this.initTops()
+            this.initScroll()
+          })
+        }
+      },
+      methods:{
+        initScroll(){
+            this.leftScroll = new betterScroll(this.$refs.left, {
+            click: true, // 分发click事件
+          })
+          // 右侧scroll对象
+          this.rightScroll = new betterScroll(this.$refs.right, {
+            click: true, // 分发click事件
+            probeType: 1  // 非实时 触摸
+            // probeType: 2  // 实时 触摸
+            // probeType: 3  // 实时 触摸/惯性/编码
+          })
+
+          // 绑定scroll监听
+          this.rightScroll.on('scroll', ({x, y}) => {
+            console.log('scroll()', x, y)
+            this.scrollY = Math.abs(y)
+          })
+
+          // 绑定scrollEnd监听
+          this.rightScroll.on('scrollEnd', ({x, y}) => {
+            console.log('scrollEnd()', x, y)
+            this.scrollY = Math.abs(y)
+          })
+        },
+        initTops(){
+          const tops = []
+          let top = 0
+          tops.push(top)
+          Array.prototype.slice.call(this.$refs.rightUl.children).forEach(li => {
+            top+=li.clientHeight
+            tops.push(top)
+          });
+          this.tops = tops
+        }
+      }
   }
 </script>
 
-<style scoped lang="stylus" rel="stylesheet/sttlus">
+<style lang="stylus" rel="stylesheet/stylus" >
+  @import "../../common/stylus/mixins.stylus"
 
- 
+  .goods
+    display: flex
+    position: absolute
+    top: 225px
+    bottom: 46px
+    width: 100%
+    background: #fff;
+    overflow: hidden
+    .menu-wrapper
+      flex: 0 0 80px
+      width: 80px
+      background: #f3f5f7
+      .menu-item
+        display: table
+        height: 54px
+        width: 56px
+        padding: 0 12px
+        line-height: 14px
+        &.current
+          position: relative
+          z-index: 10
+          margin-top: -1px
+          background: #fff
+          color: $green
+          font-weight: 700
+          .text
+            border-none()
+        .icon
+          display: inline-block
+          vertical-align: top
+          width: 12px
+          height: 12px
+          margin-right: 2px
+          background-size: 12px 12px
+          background-repeat: no-repeat
+        .text
+          display: table-cell
+          width: 56px
+          vertical-align: middle
+          bottom-border-1px(rgba(7, 17, 27, 0.1))
+          font-size: 12px
+    .foods-wrapper
+      flex: 1
+      .title
+        padding-left: 14px
+        height: 26px
+        line-height: 26px
+        border-left: 2px solid #d9dde1
+        font-size: 12px
+        color: rgb(147, 153, 159)
+        background: #f3f5f7
+      .food-item
+        display: flex
+        margin: 18px
+        padding-bottom: 18px
+        bottom-border-1px(rgba(7, 17, 27, 0.1))
+        &:last-child
+          border-none()
+          margin-bottom: 0
+        .icon
+          flex: 0 0 57px
+          margin-right: 10px
+        .content
+          flex: 1
+          .name
+            margin: 2px 0 8px 0
+            height: 14px
+            line-height: 14px
+            font-size: 14px
+            color: rgb(7, 17, 27)
+          .desc, .extra
+            line-height: 10px
+            font-size: 10px
+            color: rgb(147, 153, 159)
+          .desc
+            line-height: 12px
+            margin-bottom: 8px
+          .extra
+            .count
+              margin-right: 12px
+          .price
+            font-weight: 700
+            line-height: 24px
+            .now
+              margin-right: 8px
+              font-size: 14px
+              color: rgb(240, 20, 20)
+            .old
+              text-decoration: line-through
+              font-size: 10px
+              color: rgb(147, 153, 159)
+          .cartcontrol-wrapper
+            position: absolute
+            right: 0
+            bottom: 12px
 </style>
+
+
